f94bf09f5e82aaa2ab67f52c517dbb35505f5773
dfonsecag <dgf-95@hotmail.com>
2017-08-03 19:30:11 -0600

    seeder farmacia

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Farmacium.create(nombre: 'Administrador',corre: 'ADMIN', verificado: true, password_digest: 123456)
