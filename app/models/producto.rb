033fe9aaf8799b90d918cc390632f595f36d30ad
dfonsecag <dgf-95@hotmail.com>
2017-08-17 08:55:12 -0600

    Corregir el formulario de edicion y creacion de farmacia

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -3,7 +3,8 @@ class Producto < ActiveRecord::Base
   belongs_to :presentacion
   belongs_to :laboratorio
   belongs_to :plan_paciente
-  validates :oferta, :compra,  numericality: {  message: "Número",only_integer: true }
+  
+  validates :oferta, :compra, :numericality => true, :allow_nil => true
   
   validates :nombre,  presence:  { message: "Campo obligatorio" }
   
