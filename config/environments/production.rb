f562f0c3172bdcc97d63e5b46bdc6b1cd809f121
dfonsecag <dgf-95@hotmail.com>
2017-08-17 09:33:14 -0600

    Corregir error enviando correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,18 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address: 'smtp.gmail.com',
+     port: 587,
+     domain: 'gmail.com',
+     user_name: 'diegogarciafonseca@gmail.com',
+     password: 'df563183',
+     authentication: 'plain',
+     enable_starttls_auto: true }
+
+     
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
