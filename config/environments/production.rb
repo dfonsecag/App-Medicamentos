5edee08e754780ded8440dc91f1133c1ecd2903d
dfonsecag <dgf-95@hotmail.com>
2017-08-18 07:19:57 -0600

    arreglo enviar gmails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,10 @@ Rails.application.configure do
       # }
 
       config.action_mailer.default_url_options = { :host => 'appmedicamentos.herokuapp.com' }
+      config.action_mailer.perform_deliveries = true  
       config.action_mailer.raise_delivery_errors = true
       config.action_mailer.delivery_method = :smtp
+      config.action_mailer.default :charset => "utf-8"  
       config.action_mailer.smtp_settings = {
           :enable_starttls_auto => true,
           :address => "smtp.gmail.com",
