b651a2f54a81aaadc1a26c886d9c55202534f6fa
dfonsecag <dgf-95@hotmail.com>
2017-08-18 08:43:17 -0600

    arreglo enviar gmails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,16 +58,16 @@ Rails.application.configure do
   #    authentication: 'plain',
   #    enable_starttls_auto: true }
 
-      # config.action_mailer.default_url_options = { :host => 'gmail.com' }  
-      # config.action_mailer.delivery_method = :smtp  
-      # config.action_mailer.perform_deliveries = true  
-      # config.action_mailer.raise_delivery_errors = false  
-      # config.action_mailer.default :charset => "utf-8"  
+      config.action_mailer.default_url_options = { :host => 'appmedicamentos.herokuapp.com' }  
+      config.action_mailer.delivery_method = :smtp  
+      config.action_mailer.perform_deliveries = true  
+      config.action_mailer.raise_delivery_errors = false  
+      config.action_mailer.default :charset => "utf-8"  
       config.action_mailer.smtp_settings = {  
         address: 'smtp.gmail.com',
         port: 587,
         domain: 'gmail.com',
-        authentication: :plain,
+        authentication: 'plain',
         enable_starttls_auto: true,
         user_name: 'diegogarciafonseca@gmail.com',
         password: 'df563183'
