0ccdfafa019a4beefa3f18015f394a79995c7135
dfonsecag <dgf-95@hotmail.com>
2017-08-17 20:48:13 -0600

    Corregir error enviando correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,15 +48,26 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-     address: 'smtp.gmail.com',
-     port: 587,
-     domain: 'gmail.com',
-     user_name: 'diegogarciafonseca@gmail.com',
-     password: 'df563183',
-     authentication: 'plain',
-     enable_starttls_auto: true }
+  # config.action_mailer.delivery_method = :smtp
+  #  config.action_mailer.smtp_settings = {
+  #    address: 'smtp.gmail.com',
+  #    port: 587,
+  #    domain: 'gmail.com',
+  #    user_name: 'diegogarciafonseca@gmail.com',
+  #    password: 'df563183',
+  #    authentication: 'plain',
+  #    enable_starttls_auto: true }
+
+     ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :user_name => "diegogarciafonseca@gmail.com",
+  :password => "df563183",
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => 'none' 
+} 
 
      
 
