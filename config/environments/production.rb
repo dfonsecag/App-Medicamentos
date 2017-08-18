0dca8f751d50f3b3303f21f23d43dc6dad8930cf
dfonsecag <dgf-95@hotmail.com>
2017-08-17 21:38:52 -0600

    Corregir error enviando correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,32 +48,29 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # config.action_mailer.delivery_method = :smtp
+  #  config.action_mailer.smtp_settings = {
+  #    address: 'smtp.gmail.com',
+  #    port: 587,
+  #    domain: 'gmail.com',
+  #    user_name: 'diegogarciafonseca@gmail.com',
+  #    password: 'df563183',
+  #    authentication: 'plain',
+  #    enable_starttls_auto: true }
 
-      # config.action_mailer.default_url_options = { :host => 'appmedicamentos.herokuapp.com' }  
-      # config.action_mailer.delivery_method = :smtp  
-      # config.action_mailer.perform_deliveries = true  
-      # config.action_mailer.raise_delivery_errors = false  
-      # config.action_mailer.default :charset => "utf-8"  
-      # config.action_mailer.smtp_settings = {  
-      #   address: "smtp.gmail.com",
-      #   port: 587,
-      #   domain: "appmedicamentos.herokuapp.com",
-      #   authentication: "plain",
-      #   enable_starttls_auto: true,
-      #   user_name: "diegogarciafonseca@gmail.com",
-      #   password: "df563183"
-      # }
-
-
-      config.action_mailer.delivery_method = :smtp
-      # SMTP settings for gmail
-      config.action_mailer.smtp_settings = {
-       :address              => "smtp.gmail.com",
-       :port                 => 587,
-       :user_name            => "diegogarciafonseca@gmail.com",
-       :password             => "df563183",
-       :authentication       => "plain",
-      :enable_starttls_auto => true
+      config.action_mailer.default_url_options = { :host => 'appmedicamentos.herokuapp.com' }  
+      config.action_mailer.delivery_method = :smtp  
+      config.action_mailer.perform_deliveries = true  
+      config.action_mailer.raise_delivery_errors = false  
+      config.action_mailer.default :charset => "utf-8"  
+      config.action_mailer.smtp_settings = {  
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "appmedicamentos.herokuapp.com",
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: "diegogarciafonseca@gmail.com",
+        password: "df563183"
       }
 
      
